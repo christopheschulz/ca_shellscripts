41d9eaca96e908b1fc5e1ca572df61003406777c
Christophe Schulz <christopheschulz@orange.fr>
2025-02-05 15:08:18 +0100

    array_comparison

---
+++ array_comparison.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+	# enter your array comparison code here
+	# initialize arrays a b c
+	a=(3 5 8 10 6) 
+	b=(6 5 4 12) 
+	c=(14 7 5 7)
+	
+	for x in "${a[@]}"; do 
+	  for y in "${b[@]}"; do 
+	    if [ $x = $y ];then 
+	      z[${#z[@]}]=$x
+	    fi
+	  done 
+	done
+
+	for i in "${c[@]}"; do 
+	  for k in "${z[@]}"; do
+	    if [ $i = $k ];then
+	      j[${#j[@]}]=$i
+	    fi
+	  done 
+	done 
+
+	echo ${j[@]}
